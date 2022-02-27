6ed855ae39e0366eaa540fd5c1b488c971dc3217
Murilo Esteca Arelhano <51941771+MuriloArelhano@users.noreply.github.com>
2022-02-27 17:14:38 -0400

    feat: start rust project

---
+++ rust-version/target/rls/debug/deps/rust_version-a36042b047d39112.d
@@ -0,0 +1,5 @@
+c:\Users\muril\Documents\GitHub\HeavenEngine\rust-version\target\rls\debug\deps\rust_version-a36042b047d39112.rmeta: src\main.rs
+
+c:\Users\muril\Documents\GitHub\HeavenEngine\rust-version\target\rls\debug\deps\rust_version-a36042b047d39112.d: src\main.rs
+
+src\main.rs:
---
+++ rust-version/target/rls/debug/deps/rust_version-f1e7ab8cbbc66915.d
@@ -0,0 +1,5 @@
+c:\Users\muril\Documents\GitHub\HeavenEngine\rust-version\target\rls\debug\deps\rust_version-f1e7ab8cbbc66915.rmeta: src\main.rs
+
+c:\Users\muril\Documents\GitHub\HeavenEngine\rust-version\target\rls\debug\deps\rust_version-f1e7ab8cbbc66915.d: src\main.rs
+
+src\main.rs:
